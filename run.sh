4408034ba3a221cb5503a24d6d059e0185a67dd1
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-12-09 22:16:57 +0900

    Add kernel

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 set -xue
 
 QEMU=./qemu/build/qemu-system-riscv32
+CC=clang-16
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib -mno-relax"
 
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
